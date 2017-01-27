728620a37004b0f25892ce49bc571bd5b373eaa8
Piper Merriam <pipermerriam@gmail.com>
2017-01-27 14:54:35 -0700

    use heroku port

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.get('/*', function(req, res) {
   res.sendfile('index.html', {root: './build'})
 })
 
-app.listen(8080, function () {
-  console.log('Listening on localhost:8080')
+const port = process.env.PORT || 8080
+
+app.listen(port, function () {
+  console.log('Listening on localhost:' + port)
 })
