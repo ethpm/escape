ea8a9a0e3000b8922af478065c8d9b5b2b98ede6
Piper Merriam <pipermerriam@gmail.com>
2017-01-31 23:31:32 -0700

    home page changes

--- www-src/js/components/pages/SiteIndex.jsx
+++ www-src/js/components/pages/SiteIndex.jsx
@@ -15,7 +15,7 @@ export default connect(mapStateToProps)(React.createClass({
       <div id="landing-page">
         <div className='container'>
           <div className='row'>
-            <div id="landing-page-top" className='col-sm-7 offset-sm-5'>
+            <div id="landing-page-top" className='col-sm-8 offset-sm-2'>
 
               <div id="landing-page-top-title" className="card card-inverse">
                 <div className="card-block">
@@ -30,13 +30,15 @@ export default connect(mapStateToProps)(React.createClass({
                 <BSCard.Block>
                   <BSCard.Text>Dear Ethereum,</BSCard.Text>
 
-                  <BSCard.Text>We need to talk.  You're not the easiest platform to work with.  Don't get me wrong, you have some great qualities that we love but it's time you grew up a bit and started acting a bit more... mature.</BSCard.Text>
+                  <BSCard.Text>We need to have a talk.  You haven't been the easiest platform to work with.  Don't get me wrong, you have some great qualities that we love but it's time you grew up a bit and started acting a bit more... <em>mature</em>.</BSCard.Text>
 
-                  <BSCard.Text>Since we care about you and really want you to succeed we made you something that should help.  It's a <strong>package index</strong>.  I know that new things can be a little scary but we're sure that if you try it you'll find it's better.  Developers are going to want to play with you more.  Their bosses may even stop seeing you as the dangerous kid teaching all their devs bad software habits like copy/pasting code.</BSCard.Text>
+                  <BSCard.Text>Since we care about you and really want you to succeed we made you something that should help.  It's a <strong>package index</strong>.</BSCard.Text>
 
-                  <BSCard.Text>We hope you'll give it a try.  We really do want the best for you.</BSCard.Text>
+                  <BSCard.Text>I know that new things can be a little scary but we're sure that once you try it you'll love it.  Developers are going to want to play with you more.  Their bosses may even stop seeing you as the dangerous kid teaching all their devs bad software habits like copy/pasting code.</BSCard.Text>
 
-                  <BSCard.Text>Piper &amp; Tim</BSCard.Text>
+                  <BSCard.Text>Please give it a try.  We really do want the best for you.</BSCard.Text>
+
+                  <BSCard.Text><em>Piper &amp; Tim</em></BSCard.Text>
                 </BSCard.Block>
               </BSCard>
             </div>
