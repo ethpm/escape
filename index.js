4da0c946bc2ca442ab9359c060a17ededf5e2588
Piper Merriam <pipermerriam@gmail.com>
2017-01-31 23:06:15 -0700

    lets encrypt domain verification

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const staticRoot = process.env.STATIC_ROOT || 'build'
 
 app.use('/static', express.static(staticRoot))
 
+const LETS_ENCRYPT_SECRET_PATH = process.env.LETS_ENCRYPT_SECRET_PATH
+const LETS_ENCRYPT_SECRET = process.env.LETS_ENCRYPT_SECRET
+
+if (LETS_ENCRYPT_SECRET !== undefined && LETS_ENCRYPT_SECRET_PATH != undefined) {
+  app.get('/.well-known/acme-challenge/' + LETS_ENCRYPT_SECRET_PATH, function(req, res) {
+    res.send(LETS_ENCRYPT_SECRET)
+  })
+}
+
 app.get('/*', function(req, res) {
   res.sendFile('index.html', {root: './' + staticRoot})
 })
